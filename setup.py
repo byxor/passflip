4511c94cccdb12ae33c1a894c3a3094894cbe084
byxor <brandnewbyxor@gmail.com>
2016-10-08 18:25:27 +0100

    Remove file reading from setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,6 @@ import re
 from setuptools import setup
 
 
-with open("README.rst", "rb") as f:
-    long_description = f.read().decode("utf-8")
-
-
 setup(
     name = "passflip",
     packages = ["passflip"],
@@ -16,7 +12,6 @@ setup(
     license = "GPLv3",
     version = "0.5.7",
     description = "Command line tool to mutate passwords for different websites.",
-    long_description = long_description,
     author = "Brandon Ibbotson",
     author_email = "brandon.ibbotson2@mail.dcu.ie",
     url = "https://www.github.com/byxor/passflip"
