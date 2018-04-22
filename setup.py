fefd87a2b1daf552bbe2636b76452bcbf322c653
Brandon Ibbotson <brandnewbyxor@gmail.com>
2018-04-22 22:48:32 +0100

    Remove email address

--- setup.py
+++ setup.py
@@ -13,7 +13,6 @@ setup(
     version = "0.7",
     description = "Command line tool to mutate passwords for different websites.",
     author = "Brandon Ibbotson",
-    author_email = "brandon.ibbotson2@mail.dcu.ie",
     url = "https://www.github.com/byxor/passflip"
 )
 
